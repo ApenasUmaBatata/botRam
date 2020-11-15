990400ed3d213ae9f04bb6245273c5d0939a19b8
Glitch (hello-express) <none>
2020-11-15 18:55:15 +0000

    🚂📡 Checkpoint ./falas.json:15195554/62 ./commands/comando/botinfo.js:15195554/2427 ./commands/comando/give.js:15195554/2087

---
+++ commands/comando/botinfo.js
@@ -0,0 +1,65 @@
+const Discord = require("discord.js");
+module.exports = {
+  config: {
+    name: "botinfo",
+    aliases: ["info", "on", "convite", "invite", "bot", "ping"]
+  },
+  run: async (bot, message, args) => {
+    await message.react("✅");
+
+    let dias = 0; // variavel para definir dias
+    let week = 0; // variavel para definir semanas
+    let uptime = ``; // dentro dessa variavel, ficará armazenado todas as informações que criaremos abaixo
+    let totalSegundos = bot.uptime / 1000; // definindo o maximo de segundos, no caso 1000s
+    let horas = Math.floor(totalSegundos / 3600); // definindo o maximo de horas, no caso 3600s
+    totalSegundos %= 3600; // fazendo uma conta basica, dos segundos
+    let minutos = Math.floor(totalSegundos / 60); // um minuto e 60s
+    let segundos = Math.floor(totalSegundos % 60);
+    if (horas > 23) {
+      dias = dias + 1; // adicionando +1 dia na conta
+      horas = 0;
+    }
+    if (dias == 7) {
+      dias = 0; // zerando os dias
+      week = week + 1; // adicionando uma semana
+    }
+    if (week > 0) {
+      uptime += `${week} week, `;
+    }
+    if (minutos > 60) {
+      minutos = 0; // e zera os segundos
+    }
+    uptime += `\`${horas}h ${minutos}m ${segundos}s\``; // a finalização daquele espaco do comeco
+
+    const moment = require("moment");
+    moment.locale("pt-BR");
+    let inline = true;
+    let bicon = bot.user.displayAvatarURL();
+    let usersize = bot.users.cache.size;
+    let chansize = bot.channels.cache.size;
+    let servsize = bot.guilds.cache.size;
+    let botembed = new Discord.MessageEmbed()
+      .setColor("RANDOM")
+      .setThumbnail(bicon)
+      .setDescription("`Algumas informações sobre mim!`")
+      .addField("__Meu nome__", `**${bot.user.username}**`, inline)
+      .addField("__Meu criador__", "SrBatata#5199", inline)
+      .addField("__Meu ping__", `*${parseInt(bot.ws.ping)}*`, inline)
+      .addField(
+        "Me encontro em",
+        `${servsize} servidores com ${chansize} canais e um total de ${usersize} membros!`
+      )
+      .addField(
+        "__**Minha data de criação**__",
+        `${moment(bot.user.createdAt).format("LLLL")}!`
+      )
+      .addField("__**Atualmente tenho**__", `${bot.commands.size} comandos`)
+      .addField("__**Estou acordada há**__", `${uptime}`)
+      .addField(
+        `__**Vote em mim**__`,
+        `[Top.gg](https://top.gg/bot/705547264537657465)`
+      );
+
+    message.channel.send(botembed);
+  }
+};
---
+++ commands/comando/give.js
@@ -0,0 +1,66 @@
+const { MessageEmbed } = require("discord.js");
+const ms = require("ms");
+
+module.exports = {
+  config: {
+    name: "give",
+    aliases: ["giveaway", "sorteio"]
+  },
+  run: async (bot, message, args) => {
+    await message.react("✅");
+
+    if (
+      !message.member.hasPermission("ADMINISTRATOR") &&
+      message.author.id != "691447707134328832"
+    ) {
+      return message.reply(
+        `você precisa da permissão \`Gerenciar Mensagens\`.`
+      );
+      //seu codigo
+    }
+    if (!args[0]) return message.channel.send(`Você não especificou o tempo!`);
+    if (
+      !args[0].endsWith("d") &&
+      !args[0].endsWith("h") &&
+      !args[0].endsWith("m")
+    )
+      return message.channel.send(
+        `Você nâo usou o tempo de forma correta \n **d = dia** \n **h = hora** \n **m = minuto**`
+      );
+    if (isNaN(args[0][0])) return message.channel.send(`Isso não é um número!`);
+    let channel = message.mentions.channels.first();
+    if (!channel)
+      return message.channel.send(
+        `Você não setou um canal para que ocorra o sorteio!`
+      );
+    let prize = args.slice(2).join(" ");
+    if (!prize)
+      return message.channel.send(
+        `Impossivel completar ação, nenhum sorteio encontrado!`
+      );
+    message.channel.send(`*Sorteio criado em ${channel}*`);
+    let Embed = new MessageEmbed()
+      .setTitle(`Novo sorteio!`)
+      .setDescription(
+        `Criador do sorteio: ${message.author} \n sorteando: \n**${prize}**`
+      )
+      .setTimestamp(Date.now() + ms(args[0]))
+      .setColor(`BLUE`);
+    let m = await channel.send(Embed);
+    m.react("🎉");
+    setTimeout(() => {
+      if (m.reactions.cache.get("🎉").count <= 1) {
+        message.channel.send(`Reactions: ${m.reactions.cache.get("🎉").count}`);
+        return message.channel.send(
+          `Not enough people reacted for me to start draw a winner!`
+        );
+      }
+
+      let winner = m.reactions.cache
+        .get("🎉")
+        .users.cache.filter(u => !u.bot)
+        .random();
+      channel.send(`O ganhador do \`${prize}\` foi ${winner}`);
+    }, ms(args[0]));
+  }
+};
