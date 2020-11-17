1afcc62a446bc9b6b337bfd6e70f2d92e2d36067
Glitch (hello-express) <none>
2020-11-17 21:26:57 +0000

    🐛🎾 Checkpoint ./commands/comando/sorte.js:15195554/2573

--- commands/comando/sorte.js
+++ commands/comando/sorte.js
@@ -3,25 +3,27 @@ const Discord = require("discord.js");
 module.exports = {
   config: {
     name: "sorte",
-    aliases: ["sorteio"]
+    aliases: [""]
   },
   run: async (bot, message, args) => {
     await message.react("✅");
     var list = [
-      "Ganhou 24 moedas",
-      "Ganhou 50 moedas",
-      "Você foi sortudo e acabou da ganhar 1000 moedas",
-      "Haha sifudeu otário ganhou um total de 0 moedas",
-      "Parabéns você ganhou um diamante",
+      "vazio extremo",
+      "nadica de nada",
+      "nada",
+      "vazio extremo",
+      "nadica de nada",
+      "nada",
+      "nada",
+      "nadica de nada",
+      "vazio extremo",
+      "vazio extremo",
       "Parabéns você acabou de ganhar o ultra, mega, raro, master, espetacular, cargo secreto!",
       "nada",
       "nada",
       "vazio extremo",
       "nadica de nada",
-      "um pedaço de queijo",
-      "achou uma bala embaixo do sofá",
-      "nada",
-      "vento"
+      "nada"
     ];
 
     let altstatus = list[Math.floor(Math.random() * list.length)];
