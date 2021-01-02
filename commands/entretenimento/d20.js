f3b58d2a1285e1b9b0b8ce213fbb883b8841f6ed
Glitch (hello-express) <none>
2021-01-02 11:08:23 +0000

    🕌🦄 Checkpoint ./commands/entretenimento/d20.js:15195554/5000

--- commands/entretenimento/d20.js
+++ commands/entretenimento/d20.js
@@ -1,18 +1,24 @@
 module.exports = {
-    config: {
-      name: "d20",
-      aliases: ["dado20"]
-    },
-    run: async (bot, message, args) => {
-      await message.react("✅"); // setando a base
-  
-      // puxando um chao, com sistema randomico (de 6 a 1)
-      var numero = Math.floor(Math.random() * 20) + 1;
-  
-      message.channel.send(`Você jogou o dado e... Ele caiu em: ${numero}`);
-    setTimeout(function() {
-    
-  }, 10000);  
-  }
-}
-  
+  config: {
+    name: "d20",
+    aliases: ["dado20"]
+  },
+  run: async (bot, message, args) => {
+    await message.react("✅"); // setando a base
+
+    // puxando um chao, com sistema randomico (de 6 a 1)
+    var numero = Math.floor(Math.random() * 20) + 1;
+
+    message.channel
+      .send(`Você jogou o dado e...`)
+      .then(message => {
+        setTimeout(function() {
+          message.edit(`⠀\n Ele caiu em: ${numero}`);
+        }, 5000);
+      })
+      .then(message => {
+      setTimeout(function(){
+        message.delete(10000);
+      });
+  });
+  };
