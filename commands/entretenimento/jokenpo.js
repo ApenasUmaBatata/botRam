ffc9ee55e5503dc8c550edcf7c4ca5c918c780dc
Glitch (ram-3) <none>
2021-01-05 06:32:58 +0000

    🚒🍉 Checkpoint ./commands/entretenimento/jokenpo.js:15195554/997

--- commands/entretenimento/jokenpo.js
+++ commands/entretenimento/jokenpo.js
@@ -16,5 +16,16 @@ module.exports = {
           message.edit(`${us} você tirou \`${player[Math.floor(Math.random() * player.length)]}\` e ${target} tirou \`${player[Math.floor(Math.random() * player.length)]}\``);
         }, 5000);
       });
+      function getResult(me, target) {
+        if ((me === "🗻|pedra" && target === "✂️|tesoura") ||
+            (me === "🧻|papel" && target === "🗻|pedra") ||
+            (me === "✂️|tesoura" && target === "🧻|papel")) {
+                return "You won!";
+        } else if (me === target) {
+            return "It's a tie!";
+        } else {
+            return "You lost!";
+        }
+    }
   }
 };
