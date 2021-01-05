4a4e4a403c4cb62c4b475cf5510cafbdc5d6c325
Glitch (ram-3) <none>
2021-01-05 17:24:16 +0000

    🏦🎙 Checkpoint ./commands/entretenimento/rolar.js:15195554/98

--- commands/entretenimento/rolar.js
+++ commands/entretenimento/rolar.js
@@ -9,10 +9,9 @@ module.exports = {
       // puxando um chao, com sistema randomico (de 6 a 1)
       var d6 = Math.floor(Math.random() * 6) + 1;
       var d20 = Math.floor(Math.random() * 20) + 1;
-
       var d100 = Math.floor(Math.random() * 100) + 1;
   
-      message.channel.send(`Você jogou o dado e... Ele caiu em: ${numero}`);
+      message.channel.send(`Seus dados cairam em d6- ${d6} d20- ${d20} ${d100}`);
     }
   };
   
