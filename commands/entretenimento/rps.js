deb786950b05c39dcf50f90954bf5dff3da4c7d2
Glitch (ram-3) <none>
2021-01-05 06:44:27 +0000

    💫🎿 Checkpoint ./commands/entretenimento/jokenpo.js:15195554/893 ./commands/entretenimento/rps.js:15195554/4911

--- commands/entretenimento/rps.js
+++ commands/entretenimento/rps.js
@@ -10,11 +10,11 @@ run: async (bot, message, args) => {
         }
         let choices = ['rock', 'paper', 'scissors'];
         if (choices.includes((args[0]).toLowerCase())){
-            let number = Math.floor(Math.random() * 3);
-            if (number == 1){
+            let number = Math.floor(Math.random() * choices.length);
+            if (number == choices){
                 return message.channel.send('It was a tie , we both had '+ (args[0]).toLowerCase())    
             }
-            if (number == 2){
+            if (number == "rock"){
                 if (args[0].toLowerCase()== "rock" ){
                     return message.channel.send('I won, I had paper!')
                     }
@@ -25,7 +25,18 @@ run: async (bot, message, args) => {
                                 return message.channel.send('I won, I had rock!')
                             }
             }
-            if (number == 0){
+            if (number == "paper"){
+                if (args[0].toLowerCase()== "rock" ){
+                return message.channel.send('You won, I had scissors!')
+                }
+                    if (args[0].toLowerCase()== "paper" ){
+                        return message.channel.send('You won, I had rock!')
+                    }
+                        if (args[0].toLowerCase()== "scissors" ){
+                            return message.channel.send('You won, I had paper!')
+                        }
+            }
+            if (number == "scissors"){
                 if (args[0].toLowerCase()== "rock" ){
                 return message.channel.send('You won, I had scissors!')
                 }
