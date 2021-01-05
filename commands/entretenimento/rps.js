e6856981cc51d0f20b833fa9c30aaaf44c53ed87
Glitch (ram-3) <none>
2021-01-05 05:46:16 +0000

    📼🚃 Checkpoint ./commands/entretenimento/rps:15195554/7180 ./commands/entretenimento/rps.js:15195554/2

---
+++ commands/entretenimento/rps.js
@@ -0,0 +1,43 @@
+const Discord = require('discord.js');
+module.exports = {
+  config: {
+    name: "rps",
+    aliases: ["."]
+  },
+run: async (bot, message, args) => {
+        if (!args[1]){
+           return message.channel.send('Please include your choice')
+        }
+        let choices = ['rock', 'paper', 'scissors'];
+        if (choices.includes((args[1]).toLowerCase())){
+            let number = Math.floor(Math.random() * 3);
+            if (number == 1){
+                return message.channel.send('It was a tie , we both had '+ (args[1]).toLowerCase())    
+            }
+            if (number == 2){
+                if (args[1].toLowerCase()== "rock" ){
+                    return message.channel.send('I won, I had scissors!')
+                    }
+                        if (args[1].toLowerCase()== "paper" ){
+                            return message.channel.send('I won, I had rock!')
+                        }
+                            if (args[1].toLowerCase()== "scissors" ){
+                                return message.channel.send('I won, I had paper!')
+                            }
+            }
+            if (number == 0){
+                if (args[1].toLowerCase()== "rock" ){
+                return message.channel.send('You won, I had scissors!')
+                }
+                    if (args[1].toLowerCase()== "paper" ){
+                        return message.channel.send('You won, I had rock!')
+                    }
+                        if (args[1].toLowerCase()== "scissors" ){
+                            return message.channel.send('You won, I had paper!')
+                        }
+            }
+        } else {
+            return message.channel.send('Please include either: Rock, Paper or Scissors!')
+        }
+    }
+}
