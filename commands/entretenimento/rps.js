52d633a4fd27ebe464f3c07bfbddae2e2c78fba6
Glitch (ram-3) <none>
2021-01-06 18:38:38 +0000

    📍📯 Checkpoint ./commands/entretenimento/rps.js:15195554/324

--- commands/entretenimento/rps.js
+++ commands/entretenimento/rps.js
@@ -9,38 +9,38 @@ run: async (bot, message, args) => {
            return message.channel.send(`⠀\n Porfavor inclua sua escolha (pedra, papel ou tesoura)!\n Modo de uso: Ram jokenpo papel `)
         }
         let choices = ['pedra', 'papel', 'tesoura'];
-        if (choices.includes((args[0]))){
+        if (choices.includes((args[0]).toLowerCase())){
             let escolhas = choices[Math.floor(Math.random() * choices.length)];            
             if (escolhas == "pedra"){
-                if (args[0] == "pedra" ){
+                if (args[0]() == "pedra" ){
                     return message.channel.send('empate eu tambem tirei pedra')
                     }
-                        if (args[0] == "papel" ){
+                        if (args[0]() == "papel" ){
                             return message.channel.send('voce ganhou eu tirei pedra')
                         }
-                            if (args[0] == "tesoura" ){
+                            if (args[0]() == "tesoura" ){
                                 return message.channel.send('eu ganhei eu tirei pedra')
                             }
             }
             if (escolhas == "papel"){
-                if (args[0] == "pedra" ){
+                if (args[0].toLowerCase() == "pedra" ){
                 return message.channel.send('eu ganhei eu tirei papel')
                 }
-                    if (args[0] == "papel" ){
+                    if (args[0].toLowerCase() == "papel" ){
                         return message.channel.send('empate eu tambem tirei papel')
                     }
-                        if (args[0] == "tesoura" ){
+                        if (args[0].toLowerCase() == "tesoura" ){
                             return message.channel.send('voce ganhou eu tirei papel')
                         }
             }
             if (escolhas == "tesoura"){
-                if (args[0] == "pedra" ){
+                if (args[0].toLowerCase() == "pedra" ){
                 return message.channel.send('voce ganhou eu tirei tesoura')
                 }
-                    if (args[0] == "papel" ){
+                    if (args[0].toLowerCase() == "papel" ){
                         return message.channel.send('eu ganhei eu tirei tesoura')
                     }
-                        if (args[0] == "tesoura" ){
+                        if (args[0].toLowerCase() == "tesoura" ){
                             return message.channel.send('empate eu tambem tirei tesoura')
                         }
             }
