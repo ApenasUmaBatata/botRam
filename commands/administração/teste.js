0ab047748cf55928cebf9a7f6ec334b06f66f53b
Glitch (ram-3) <none>
2021-01-07 00:11:12 +0000

    🐙⛸ Checkpoint ./commands/rpg/rolar.js:15195554/98 ./commands/administração/teste.js:15195554/1802

--- commands/administração/teste.js
+++ commands/administração/teste.js
@@ -1,18 +1,16 @@
 module.exports = {
   config: {
-    name: "roll",
-    aliases: ["rolar"]
+    name: "teste",
+    aliases: ["teste"]
   },
   run: async (bot, message, args) => {
     //await message.react("✅"); // setando a base
 
     // puxando um chao, com sistema randomico (de 6 a 1)
-    var d6 = Math.floor(Math.random() * 6) + 1;
-    var d20 = Math.floor(Math.random() * 20) + 1;
-    var d100 = Math.floor(Math.random() * 100) + 1;
+
 
     message.channel
-      .send(`Seus dados cairam em d6- ${d6} d20- ${d20} ${d100}`)
+      .send(`Seus dados cairam em d6-`)
       .then(msg => {
         setTimeout(function() {
           msg.edit("Seus dados cairam em 1").then(msg => {
