9acc49126e63e363ad0a120280b2a2bc71b9c357
Glitch (ram-3) <none>
2021-01-07 00:34:19 +0000

    👋📍 Checkpoint ./commands/administração/teste.js:15195554/11542

--- commands/administração/teste.js
+++ commands/administração/teste.js
@@ -8,32 +8,116 @@ module.exports = {
 
     // puxando um chao, com sistema randomico (de 6 a 1)
 
-
-    message.channel
-      .send(`Seus dados cairam em d6-`)
-      .then(msg => {
-        setTimeout(function() {
-          msg.edit("Seus dados cairam em 1").then(msg => {
-            setTimeout(function() {
-              msg.edit("Seus dados cairam em 1 \n Seus dados cairam em 2")
-                .then(msg => {
-                  setTimeout(function() {
-                    msg.edit("Seus dados cairam em 1 \n Seus dados cairam em 2 \n Seus dados cairam em 3")
-                      .then(msg => {
-                        setTimeout(function() {
-                          msg.edit("Seus dados cairam em 1 \n Seus dados cairam em 2 \n Seus dados cairam em 3 \n Seus dados cairam em 4")
-                            .then(msg => {
-                              setTimeout(function() {
-                                msg.edit("Seus dados cairam em 1 \n Seus dados cairam em 2 \n Seus dados cairam em 3 \n Seus dados cairam em 4 \n Seus dados cairam em 5");
-                              }, 3000);
+    message.channel.send(`Seus dados cairam em d6-`).then(msg => {
+      setTimeout(function() {
+        msg.edit("Seus dados cairam em 1").then(msg => {
+          setTimeout(function() {
+            msg
+              .edit("Seus dados cairam em 1 \n Seus dados cairam em 2")
+              .then(msg => {
+                setTimeout(function() {
+                  msg
+                    .edit(
+                      "Seus dados cairam em 1 \n Seus dados cairam em 2 \n Seus dados cairam em 3"
+                    )
+                    .then(msg => {
+                      setTimeout(function() {
+                        msg
+                          .edit(
+                            "Seus dados cairam em 1 \n Seus dados cairam em 2 \n Seus dados cairam em 3 \n Seus dados cairam em 4"
+                          )
+                          .then(msg => {
+                            setTimeout(function() {
+                              msg
+                                .edit(
+                                  "Seus dados cairam em 1 \n Seus dados cairam em 2 \n Seus dados cairam em 3 \n Seus dados cairam em 4 \n Seus dados cairam em 5"
+                                )
+                                .then(msg => {
+                                  setTimeout(function() {
+                                    msg
+                                      .edit("Seus dados cairam em 1")
+                                      .then(msg => {
+                                        setTimeout(function() {
+                                          msg
+                                            .edit(
+                                              "Seus dados cairam em 1 \n Seus dados cairam em 2"
+                                            )
+                                            .then(msg => {
+                                              setTimeout(function() {
+                                                msg
+                                                  .edit(
+                                                    "Seus dados cairam em 1"
+                                                  )
+                                                  .then(msg => {
+                                                    setTimeout(function() {
+                                                      msg
+                                                        .edit(
+                                                          "Seus dados cairam em 1 \n Seus dados cairam em 2"
+                                                        )
+                                                        .then(msg => {
+                                                          setTimeout(
+                                                            function() {
+                                                              msg
+                                                                .edit(
+                                                                  "Seus dados cairam em 1"
+                                                                )
+                                                                .then(msg => {
+                                                                  setTimeout(
+                                                                    function() {
+                                                                      msg
+                                                                        .edit(
+                                                                          "Seus dados cairam em 1 \n Seus dados cairam em 2"
+                                                                        )
+                                                                        .then(
+                                                                          msg => {
+                                                                            setTimeout(
+                                                                              function() {
+                                                                                msg
+                                                                                  .edit(
+                                                                                    "Seus dados cairam em 1"
+                                                                                  )
+                                                                                  .then(
+                                                                                    msg => {
+                                                                                      setTimeout(
+                                                                                        function() {
+                                                                                          msg.edit(
+                                                                                            "Seus dados cairam em 1 \n Seus dados cairam em 2"
+                                                                                          );
+                                                                                        },
+                                                                                        3000
+                                                                                      );
+                                                                                    }
+                                                                                  );
+                                                                              },
+                                                                              3000
+                                                                            );
+                                                                          }
+                                                                        );
+                                                                    },
+                                                                    3000
+                                                                  );
+                                                                });
+                                                            },
+                                                            3000
+                                                          );
+                                                        });
+                                                    }, 3000);
+                                                  });
+                                              });
+                                            });
+                                        });
+                                      });
+                                  });
+                                });
                             });
-                        }, 3000);
+                          });
                       });
-                  }, 3000);
+                    });
                 });
-            }, 3000);
+              });
           });
-        }, 3000);
+        });
       });
+    });
   }
 };
