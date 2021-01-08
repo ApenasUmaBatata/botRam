0b17e30efe8551ceb44c74fd829501ff6b41430e
Glitch (ram-3) <none>
2021-01-08 03:36:39 +0000

    🏤🚠 Checkpoint ./commands/comando/filme.js:15195554/393 ./commands/comando/enquete.js:15195554/8

--- commands/comando/filme.js
+++ commands/comando/filme.js
@@ -9,15 +9,18 @@ module.exports = {
            return message.channel.send(`⠀\n  `)
         }
     
+    var film = args.slice(0).join("1 \n 2")
+    
     let embed = new Discord.MessageEmbed()
       .setTitle(`ENQUETE`)
       .setColor("GREEN")
-      .setDescription(``)
+      .setDescription(`${film}`)
       .setFooter(`Clique em um emoji para deixar sua opinião!`)
-    message.channel.send(embed).then(function(msg) {
-      msg.react("<:like1:795044874515906602>");
-      msg.react("<:dislike1:795044933605785600>");
-    });
+    message.channel.send(embed)
+      //.then(function(msg) {
+      //msg.react("<:like1:795044874515906602>");
+      //msg.react("<:dislike1:795044933605785600>");
+    //});
     message.delete();
   }
 }
