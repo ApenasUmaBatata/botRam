0491e3861e82420fb5ed563433d175c651ae97a5
Glitch (ram-3) <none>
2021-01-09 01:16:15 +0000

    🌆📅 Checkpoint ./commands/comando/filme.js:15195554/3661

--- commands/comando/filme.js
+++ commands/comando/filme.js
@@ -6,8 +6,16 @@ module.exports = {
   },
   run: async (bot, message, args) => {
     if (!args[0]) {
-      return message.channel.send(`Exemplo de uso: \n Ram sfilme bob_esponja moranguinho vovo_zona o_bom_dinossauro \n *OBS*: filmes que `);
+      let eb = new Discord.MessageEmbed()
+        .setTitle(`Exemplo de uso:`)
+        .setDescription(
+          `Ram sfilme bob-esponja moranguinho vovo-zona o-bom-dinossauro \n \`OBS-1: filmes que contenham nome composto, usar ao invés de "espaço" colocar -\` \n \`OBS-2: na troca de um filme para o outro usar "espaço"\` \n \`OBS-3: Limite máximo de 4 filmes!\``
+        );
+      return message.channel.send(eb);
+      ;
     }
+    message.delete()
+    
     var a1 = args[0];
     var a2 = args[1];
     var a3 = args[2];
@@ -16,7 +24,9 @@ module.exports = {
     let embed = new Discord.MessageEmbed()
       .setTitle(`Filmes sugeridos:`)
       .setColor("RED")
-      .setDescription(` <:um:797245701578555474>- ${a1} \n <:dois:797246681384353802>- ${a2} \n <:tres:797248664011669504>- ${a3} \n ${a4}`)
+      .setDescription(
+        ` <:um:797245701578555474>- ${a1} \n <:dois:797246681384353802>- ${a2} \n <:tres:797248664011669504>- ${a3} \n ${a4}`
+      )
       .setFooter(`Clique em um emoji para deixar sua opinião!`);
     message.channel.send(embed).then(function(msg) {
       msg.react("<:um:797245701578555474>");
