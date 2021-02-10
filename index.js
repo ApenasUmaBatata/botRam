26d85a59801e41d56ff74ce3ca9ddd132cbc198f
Glitch (ram-3) <none>
2021-02-10 18:02:22 -0300

    mudança index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { Client, Collection } = require("discord.js");
 
 const bot = new Client();
-bot.queue = new Map();
 
 ["aliases", "commands",].forEach(x => bot[x] = new Collection());
 ["command", "event"].forEach(x => require(`./handlers/${x}`)(bot));
