ff5e8649dccad556642cde20845ceef9be42a747
Glitch (ram-3) <none>
2021-03-02 19:46:05 -0300

    removendo handler banco de dados

--- handlers/command.js
+++ handlers/command.js
@@ -12,5 +12,5 @@ module.exports = bot => {
         pull.config.aliases.forEach(a => bot.aliases.set(a, pull.config.name));
     }
   };
-  ["comando", "entretenimento", "administração", "economia", "rpg"].forEach(x => load(x));
+  ["comando", "entretenimento", "administração", "rpg"].forEach(x => load(x));
 };
