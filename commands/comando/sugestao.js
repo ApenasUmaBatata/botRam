ed35a306f7d15753a68eada8c7a3cc54fdf53ac8
Glitch (ram-3) <none>
2021-08-20 00:50:37 -0300

    comando de sugestão criado

---
+++ commands/comando/sugestao.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    config: {
+      name: "sugestão",
+      aliases: ["sugest", "su"]
+    },
+    run: async (bot, message, args) => {
+
+  // var canal = client.channels.get('692026509841662073')
+
+  var sugestao = args.slice(0).join(" ");
+  if (!args.join(" ")) return message.reply("Escreva sua sugestão!");
+    const embed = new Discord.MessageEmbed()
+      .setColor("RANDOM")
+      .setDescription(`O membro \`${message.author.username}\` de \`${message.guild.name}\` deu a seguinte sugestão -> \`${sugestao}\``)
+      .setColor("RANDOM")
+    bot.channels.cache.get("878089287072108545").send({ embeds:[embed] })
+    const embed2 = new Discord.MessageEmbed()
+      .setColor("RANDOM")
+      .setDescription(`Sua sugestão -> \`${sugestao}\``)
+      .setColor("RANDOM")
+    message.reply({embeds: [embed2]})
+    message.reply(`Sua sugestão foi enviada para ${bot.channels.cache.get("878089287072108545")}! :mailbox_with_no_mail:`);
+}
+}
